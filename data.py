250dac6178b4978e9511937855ebdb9e3f0052af
Hamza Bhatti <hbhati006@gmail.com>
2021-04-23 15:50:23 +0500

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'name': 'Article One',
+            'body': 'lorem Ipson Dolor ajhhak ufhkjhfks fjskjfjks fhsjkf ',
+            'author': 'Hamza Bhatti',
+            'create_date': '04-25-2021'
+        },
+        {
+            'id': 1,
+            'name': 'Article Two',
+            'body': 'lorem Ipson Dolor ajhhak ufhkjhfks fjskjfjks fhsjkf ',
+            'author': 'Bilal Bhatti',
+            'create_date': '04-25-2021'
+        },
+        {
+            'id': 1,
+            'name': 'Article Three',
+            'body': 'lorem Ipson Dolor ajhhak ufhkjhfks fjskjfjks fhsjkf ',
+            'author': 'Kashaf',
+            'create_date': '04-25-2021'
+        },
+        
+    ]
+    return articles
